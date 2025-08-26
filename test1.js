366cb965357e2234dd52dbee6df0885fb1478f95
keerthisiva <sskeerthi09@gmail.com>
2025-08-26 19:57:59 +0530

    Create test1.js
    initila commit

---
+++ test1.js
@@ -0,0 +1,2 @@
+this is a test file, normally we are creating file from vs code, i dont have permission to push my changes to this repo. i configured different account with my git which installed in my machine, if i change the user name and password only i can able to push to this repo. so i am creating file from here
+to explain the pull request.
